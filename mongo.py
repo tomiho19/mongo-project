ab3463c62d4c6de2bc99a30c3a3495bd50f39003
alex.sidorenko <alex.sidorenko@lexico-voip.com>
2019-12-10 23:28:04 +0200

    v0.0.1

--- mongo.py
+++ mongo.py
@@ -16,9 +16,7 @@ CORS(app)
 @app.route('/api/friends', methods=['GET'])
 def get_all_friends():
     friends = mongo.db.friends
-
     result = []
-
     for field in friends.find():
         result.append({'_id': str(field['_id']), 'title': field['title']})
     return jsonify(result)
@@ -27,40 +25,51 @@ def get_all_friends():
 @app.route('/api/friend', methods=['POST'])
 def add_friend():
     friends = mongo.db.friends
-    title = request.get_json()['title']
+    first_name = request.get_json()['first_name']
+    last_name = request.get_json()['last_name']
+    email = request.get_json()['email']
+    password = request.get_json()['password']
+    birth_date = request.get_json()['birth_date']
 
-    friend_id = friends.insert({'title': title})
+    friend_id = friends.insert({'first_name': first_name,
+                                'last_name': last_name,
+                                'email': email,
+                                'password': password,
+                                'birth_date': birth_date})
     new_friend = friends.find_one({'_id': friend_id})
-
-    result = {'title': new_friend['title']}
-
+    result = {'id': new_friend['id']}
     return jsonify({'reuslt': result})
 
 
 @app.route('/api/friend/<id>', methods=['PUT'])
 def update_friend(id):
     friends = mongo.db.friends
-    title = request.get_json()['title']
+    first_name = request.get_json()['first_name']
+    last_name = request.get_json()['last_name']
+    email = request.get_json()['email']
+    password = request.get_json()['password']
+    birth_date = request.get_json()['birth_date']
 
-    friends.find_one_and_update({'_id': ObjectId(id)}, {"$set": {"title": title}}, upsert=False)
+    friends.find_one_and_update({'_id': ObjectId(id)}, {
+        "$set": {'first_name': first_name,
+                 'last_name': last_name,
+                 'email': email,
+                 'password': password,
+                 'birth_date': birth_date
+                 }}, upsert=False)
     new_friend = friends.find_one({'_id': ObjectId(id)})
-
-    result = {'title': new_friend['title']}
-
+    result = {'id': new_friend['id']}
     return jsonify({"result": result})
 
 
 @app.route('/api/friend/<id>', methods=['DELETE'])
 def delete_friend(id):
     friends = mongo.db.friends
-
     response = friends.delete_one({'_id': ObjectId(id)})
-
     if response.deleted_count == 1:
         result = {'message': 'record deleted'}
     else:
         result = {'message': 'no record found'}
-
     return jsonify({'result': result})
 
 
